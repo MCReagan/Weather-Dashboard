0f0563d8ac1fe7fdb5d19c8707a200b0dc781f1a
MCReagan <127553558+MCReagan@users.noreply.github.com>
2023-04-14 22:14:43 -0500

    console logged all pertinent info

--- assets/script.js
+++ assets/script.js
@@ -2,8 +2,10 @@ const APIKey = "2407265d623bf7d962c05a67c5a0fc7d";
 
 var searchBar = document.getElementById('searchBar');
 var searchButton = document.getElementById('searchButton');
+var forecast = document.getElementsByClassName('forecast');
+
 // need a function to gather search input for user and save to local storage
-searchButton.addEventListener('click', function() {
+searchButton.addEventListener('click', function () {
     if (searchBar.value) {
         localStorage.setItem("city", searchBar.value);
         searchBar.value = "";
@@ -19,9 +21,24 @@ searchButton.addEventListener('click', function() {
 function getWeather(cityName) {
     var cityName = localStorage.getItem("city");
     var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + cityName + "&appid=" + APIKey;
-    console.log(queryURL);
     fetch(queryURL)
         .then(function (response) {
-            console.log(response);
+            return response.json();
+        })
+        .then(function (data) {
+            for (var i = 0; i < data.list.length; i++) {
+                console.log(data.list[i]);
+                console.log(data.list[i].dt_txt);
+                console.log(data.list[i].main.temp);
+                console.log(data.list[i].wind.speed);
+                console.log(data.list[i].main.humidity);
+                console.log(data.list[i].weather[0].icon);
+
+                
+                // var createTable = document.createElement('table');
+                // var tableRow = document.createElement('tr');
+
+                // forecast.append(data.list[i]. )
+            }
         })
 };
